d785fe4f9ab2f3af177edc91c1925b8b2597b883
olivia1117 <oadamic@gmail.com>
2024-03-01 17:51:15 -0600

    adding index file

---
+++ index.js
@@ -0,0 +1,13 @@
+fetch("", {
+  method: "POST",
+  body: JSON.stringify({
+    userId: 1,
+    title: "Fix my bugs",
+    completed: false
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));

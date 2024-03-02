e99454100404938e505e22fc30b3f4ae64fac1f2
olivia1117 <oadamic@gmail.com>
2024-03-01 19:20:24 -0600

    test 2 adding /age

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let value = 10;
-fetch("https://health-insurance-risk-calculator-server.azurewebsites.net", {
+fetch("https://health-insurance-risk-calculator-server.azurewebsites.net/age", {
   method: "POST",
   body: JSON.stringify({
     age: value,

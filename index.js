349308f4bfb3510e43d1bb247d724751004687b1
Josh Brown <pokeboy99323@gmail.com>
2024-03-05 14:57:32 -0600

    Adding age api for client

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-fetch("https://health-insurance-risk-calculator-server.azurewebsites.net", {
-  method: "POST",
-  body: JSON.stringify({
-    age: age-question,
-    title: "Fix my bugs",
-    completed: false
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+async function age() {
+  const age = document.getElementById("age-question").value
+
+  // Note the URL (fetch) string will look something like '/add-two-integers?x=2&y=1' when it is sent.
+  const url = "health-insurance-risk-calculator-server.azurewebsites.net"
+  const fetchString = url+"/age?age="+age
+  const response = await fetch(fetchString)
+  const responseText = await response.text()
+  document.getElementById("age-points").value = responseText
+  console.log(document.getElementById("age-points").value)
+}

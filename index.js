e6823e3029397a177e53562c34d2a1f1aad6938d
olivia1117 <oadamic@gmail.com>
2024-03-06 12:06:18 -0600

    making the links work to pull from the correct url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function age() {
   const age = document.getElementById("age-question").value
 
   // Note the URL (fetch) string will look something like '/add-two-integers?x=2&y=1' when it is sent.
-  const url = "health-insurance-risk-calculator-server.azurewebsites.net"
+  const url = "https://health-insurance-risk-calculator-server.azurewebsites.net"
   const fetchString = url+"/age?age="+age
   const response = await fetch(fetchString)
   const responseText = await response.text()
@@ -10,18 +10,18 @@ async function age() {
   console.log(document.getElementById("age-points").value)
 }
 
-// async function bmi(){
-//   const height = document.getElementById("height").value
-//   const weight = document.getElementById("weight").value
-//   const url = "health-insurance-risk-calculator-server.azurewebsites.net"
-//   const fetchString = url + "/bmi?height=" + height + "&weight=" + weight
-//   const response = await fetch(fetchString)
-//   const responseTextBMI = await response.text()
+async function bmi(){
+  const height = document.getElementById("height").value
+  const weight = document.getElementById("weight").value
+  const url = "https://health-insurance-risk-calculator-server.azurewebsites.net"
+  const fetchString = url + "/bmi?height=" + height + "&weight=" + weight
+  const response = await fetch(fetchString)
+  const responseTextBMI = await response.text()
 
-//   //change this to be the result of the bmi calculation 
-//   document.getElementById("bmi-result").value = responseTextBMI
-//   console.log(document.getElementById("bmi-result").value)
+  //change this to be the result of the bmi calculation 
+  document.getElementById("bmi-result").value = responseTextBMI
+  console.log(document.getElementById("bmi-result").value)
 
 
 
-// }
+}

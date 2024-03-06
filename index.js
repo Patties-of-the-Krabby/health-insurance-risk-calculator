364fe6043591652b5a9c08ed46184f8962d9f221
olivia1117 <oadamic@gmail.com>
2024-03-05 20:58:49 -0600

    updating fetch string and html value

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ async function bmi(){
   const height = document.getElementById("height").value
   const weight = document.getElementById("weight").value
   const url = "health-insurance-risk-calculator-server.azurewebsites.net"
-  const fetchString = url+"/bmi?bmi="+bmi
+  const fetchString = url + "/bmi?height=" + height + "&weight=" + weight
   const response = await fetch(fetchString)
-  const responseText = await response.text()
+  const responseTextBMI = await response.text()
 
   //change this to be the result of the bmi calculation 
-  document.getElementById("bmi-result").value = responseText
+  document.getElementById("bmi-result").value = responseTextBMI
   console.log(document.getElementById("bmi-result").value)
+
+
 
 }
